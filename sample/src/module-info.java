a0526ea98fca6c6f969640d49248b84cedc10a67
Khatavakar <aniket.khatavakar@capgemini.com>
2023-05-22 12:35:44 +0530

    java sample program

---
+++ sample/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author AKHATAVA
+ *
+ */
+module sample {
+}
---
+++ sample/src/sample/sample.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class sample {
+
+	public static void main(String[] args) {
+		System.out.println("heloo");
+		System.out.println("helon wrold");
+		System.out.println("helon wrold");
+		System.out.println("helon wrold");
+		
+
+	}
+
+}
